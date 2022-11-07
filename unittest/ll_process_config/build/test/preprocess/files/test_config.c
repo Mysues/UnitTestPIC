af9acfa20f686d94990748f96e1a3fe02aa2e15a
uthman1 <uthman@e-shore.com.my>
2022-11-07 08:52:40 +0800

    add ceedling project

---
+++ unittest/ll_process_config/build/test/preprocess/files/test_config.c
@@ -0,0 +1,27 @@
+#include "../../header/ll_process_config.h"
+#include "C:/Ruby27-x64/lib/ruby/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
+
+
+
+
+void setUp(void)
+
+{
+
+}
+
+
+
+void tearDown()
+
+{
+
+}
+
+
+
+void test_SquareOfFive(void)
+
+{
+
+}
---
+++ unittest/ll_process_config/test/test_config.c
@@ -0,0 +1,15 @@
+/* code/test/test_rechner.c */
+#include "unity.h"
+#include "ll_process_config.h"
+
+void setUp(void)
+{
+}
+
+void tearDown()
+{
+}
+
+void test_SquareOfFive(void)
+{
+}
